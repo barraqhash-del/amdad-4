fdbecc32668697b8ae38f9e1b7971361af3cf30d
barraqhash-del <barraqhash@gmail.com>
2026-08-19 18:25:19 +0300

    fix: initialize realtime localStorage synchronization

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { startRealtimeStoreSync } from './services/realtimeSync';
+
+startRealtimeStoreSync();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
